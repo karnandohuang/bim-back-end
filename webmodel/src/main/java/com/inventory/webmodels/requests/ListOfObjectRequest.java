b8e7a4fac329bcbba28fd42cf0df92ed044d533b
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2018-11-04 16:48:03 +0700

    BACK-14 add listOfObject class
    add listOfObject class

---
+++ webmodel/src/main/java/com/inventory/webmodels/requests/ListOfObjectRequest.java
@@ -0,0 +1,17 @@
+package com.inventory.webmodels.requests;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+public class ListOfObjectRequest {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String name;
+
+    private int pageNumber;
+    private int pageSize;
+    private String sortedBy;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String sortedType;
+}
