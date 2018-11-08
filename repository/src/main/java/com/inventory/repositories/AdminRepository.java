608219e6c4a1dd93609acc304a0290b5490af4df
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2018-11-09 00:07:57 +0700

    BACK-26 fix little error of admin repo
    fix little error of admin repo

--- repository/src/main/java/com/inventory/repositories/AdminRepository.java
+++ repository/src/main/java/com/inventory/repositories/AdminRepository.java
@@ -2,6 +2,8 @@ package com.inventory.repositories;
 
 import com.inventory.models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AdminRepository extends JpaRepository<String, Admin> {
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, String> {
 }
